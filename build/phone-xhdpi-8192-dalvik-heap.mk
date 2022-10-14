6c0912ae41266628cdbaba64d26b8b99fd35af79
minaripenguin37 <alexfinhart@gmail.com>
2022-10-15 03:47:49 +0800

    build: Sync dalvik heap configs with cheetah
    https://dumps.tadiphone.dev/dumps/google/cheetah/-/blob/cheetah-user-13-TD1A.220804.009.A2-8940162-release-keys/vendor/build.prop#L74

--- build/phone-xhdpi-8192-dalvik-heap.mk
+++ build/phone-xhdpi-8192-dalvik-heap.mk
@@ -20,8 +20,8 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heaptargetutilization=0.5 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=32m
